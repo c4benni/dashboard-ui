2aa0d381fb9fbe7014be2f96ba0b63a5df542ffd
c4benni <c4benni@gmail.com>
2022-02-07 14:44:17 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,92 @@
+const description = 'A minimal Dashboard interface by C4Benni'
+
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'dashboard-ui',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'dashboard-ui',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+
+            {
+                hid: 'description',
+                name: 'description',
+                content: description,
+            },
+            {
+                hid: 'twitter-app-country',
+                property: 'twitter:app:country',
+                content: 'NG',
+            },
+            {
+                hid: 'twitter-site',
+                property: 'twitter:site',
+                content: '@c4benn',
+            },
+            {
+                hid: 'twitter-image',
+                property: 'twitter:image',
+                content: '~static/icon.png',
+            },
+            {
+                hid: 'twitter-card',
+                property: 'twitter:card',
+                content: 'summary_large_image',
+            },
+            {
+                hid: 'twitter-title',
+                property: 'twitter:title',
+                content: description,
+            },
+            {
+                hid: 'twitter-desc',
+                property: 'twitter:description',
+                content: description,
+            },
+        ],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.png',
+        }, ],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: [{ path: '~/components', extensions: ['vue'] }],
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: ['@nuxtjs/pwa', 'portal-vue/nuxt'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    pwa: {
+        manifest: {
+            lang: 'en',
+        },
+    },
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    server: {
+        port: 8008, // default: 3000
+        host: '0.0.0.0', // default: localhost
+    },
 }
