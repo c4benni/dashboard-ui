f497bc37b7e9d82cf6ef1a048050d4cde7815686
c4benni <c4benni@gmail.com>
2022-02-08 09:35:58 +0100

    Create UiText
    This component is a helper for easily adding typography classes.
    Text element was reserved.

---
+++ components/UiText/index.vue
@@ -0,0 +1,74 @@
+<script>
+import { trimmedLowerCase } from '../utils';
+// A functional utitlity component to render different typography based on props passed;
+// handles font-weight, font-size, and tag to be rendered;
+// color will be handled externally;
+
+export default {
+  name: 'UiText',
+  props: {
+    tag: {
+      type: String,
+      default: 'p',
+    },
+    size: {
+      type: String,
+      default: 'md',
+      validator:(prop)=> ['xs','sm','md','lg','xl','2xl'].includes(trimmedLowerCase(prop))
+    },
+    weight: {
+      type: String,
+      default: '400',
+      validator:(prop)=> ['400', '500', '600'].includes(prop.trim())
+    },
+  },
+  render(h) {
+    if(!this.$slots.default){
+      return null
+    }
+
+    // get the tailwind class to be applied based on the `size` prop;
+    // returns md size if size not given or passed a wrong prop value;
+    // if `size` === 'md', returns ''. To avoid unecessary className;
+    const getSize = () => {
+      switch (trimmedLowerCase(this.size)) {
+        case 'xs':
+          return 'text-xs';
+        case 'sm':
+          return 'text-sm'
+        case 'lg':
+          return 'text-lg'
+        case 'xl':
+          return 'text-xl'
+        case '2xl':
+          return 'text-2xl'
+        default:
+          return ''
+      }
+    }
+
+    // get the tailwind class to be applied based on the `weight` prop;
+    // if `weight` === 400, returns ''. To avoid unecessary className;
+    // defaults to 400;
+    const getWeight = () => {
+      switch (this.weight.trim()) {
+        case '500':
+          return 'font-medium'
+        case '600':
+          return 'font-semibold'
+        default:
+          return ''
+      }
+    }
+
+    return h(
+      this.tag,
+      {
+        ...this.$attrs,
+        class: [getSize(), getWeight()],
+      },
+      [this.$slots.default]
+    )
+  },
+}
+</script>
