fd65f238d701ef32c24a84bf750693482405d861
c4benni <c4benni@gmail.com>
2022-02-08 17:09:30 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false,
+    },
+    extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+    plugins: [],
+    // add your custom rules here
+    rules: {
+        'no-console': 'off',
+    },
 }
