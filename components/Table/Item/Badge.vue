e548e2e66f2a7e591495be9f54f85162cdaf7095
c4benni <c4benni@gmail.com>
2022-02-08 23:44:10 +0100

    Create TableItemBadge

---
+++ components/Table/Item/Badge.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="inline-flex h-11 py-1 px-4 rounded-xl justify-center items-center text-xs font-medium select-none"
+    :class="[
+      {
+        'bg-success-50 text-success-700': success,
+        'bg-gray-100 text-gray-700': !success,
+      },
+    ]"
+  >
+    {{ label }}
+  </div>
+</template>
+
+<script>
+import { requiredStringProp } from '~/components/utils'
+export default {
+  name: 'TableItemBadge',
+  props: {
+    label: requiredStringProp,
+    success: Boolean,
+  },
+}
+</script>
