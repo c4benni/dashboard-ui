381779560f5100f0031f4fdef6de4292c119990f
c4benni <c4benni@gmail.com>
2022-02-08 17:13:49 +0100

    Update utils/index

--- components/utils/index.js
+++ components/utils/index.js
@@ -12,3 +12,29 @@ export const requiredStringProp = requiredProp(String)
 export const undefinedStringProp = undefinedProp(String)
 
 export const trimmedLowerCase = (string) => string.toLowerCase().trim()
+
+export function capitalize(string) {
+    if (typeof string === 'string') {
+        return `${string.slice(0, 1).toUpperCase()}${string.slice(1)}`
+    }
+    return ''
+}
+
+export function camelCase(string) {
+    if (typeof string === 'string') {
+        return `${string.slice(0, 1).toLowerCase()}${string
+      .slice(1)
+      .replace(/-[a-z]/g, (x) => `${x.slice(1)}`.toUpperCase())}`
+  }
+  return ''
+}
+
+export function kebabCase(string) {
+  if (typeof string === 'string') {
+    return string
+      .replace(/[A-Z]/g, (x) => `-${x}`)
+      .replace(/^-|-$/g, '')
+      .toLowerCase()
+  }
+  return ''
+}
