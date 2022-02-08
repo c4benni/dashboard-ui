eaaa301393de2069a3f6213c7fb8a6c6533e7af0
c4benni <c4benni@gmail.com>
2022-02-08 09:33:18 +0100

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+const plugin = require('tailwindcss/plugin')
 const theme = require('./tailwind.theme')
 
 module.exports = {
     mode: 'jit',
-    purge: [
+    content: [
         './components/**/*.{vue,js}',
         './layouts/**/*.vue',
         './pages/**/*.vue',
@@ -11,5 +12,17 @@ module.exports = {
     ],
     darkMode: 'class',
     theme,
+    plugins: [
+        plugin(function({ addVariant, e }) {
+            addVariant('can-hover', ['@media (pointer: fine) and (hover: hover)'])
+            addVariant('no-hover', ['@media (pointer: coarse) and (hover: none)'])
+            addVariant('supports-blur', ['@supports (backdrop-filter: blur(1px))'])
+            addVariant('not-supports-blur', [
+                '@supports not (backdrop-filter: blur(1px))',
+            ])
+            addVariant('landscape', '@media (orientation: landscape)')
+            addVariant('portrait', '@media (orientation: portrait)')
+        }),
+    ],
     //   important: true,
 }
