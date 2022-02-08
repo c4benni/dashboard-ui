ab656459e3f43f18ff5ba287369e0fb07eac583f
c4benni <c4benni@gmail.com>
2022-02-08 20:57:53 +0100

    Create MetricGroup.vue

---
+++ components/MetricGroup/index.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <ul
+    class="grid md:grid-flow-col gap-10 md:gap-12"
+  >
+    <slot/>
+  </ul>
+</template>
+
+<script>
+export default {
+    name: 'MetricGroup'
+}
+</script>
