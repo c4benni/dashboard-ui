b0da5207c813c5f63e7e039814a6725fd594b5f3
c4benni <c4benni@gmail.com>
2022-02-08 20:58:10 +0100

    Create Nav.vue

---
+++ components/Nav/index.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <nav>
+      <ul class="h-20 grid grid-flow-col gap-x-2 justify-start">
+          <slot/>
+      </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'Nav'
+}
+</script>
