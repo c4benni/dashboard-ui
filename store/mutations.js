76fcc83e3c318f782a214725b0d82e974220eedc
c4benni <c4benni@gmail.com>
2022-02-10 00:01:02 +0100

    Update mutations.js

--- store/mutations.js
+++ store/mutations.js
@@ -2,4 +2,13 @@ export default {
     BREAKPOINT(s, p) {
         s.breakpoint = p
     },
+    SELECTED_TABLE_ITEM_INDEX(s, p) {
+        s.selectedTableItemIndex = p
+    },
+    TABLE_SORT_STEP(s, p) {
+        s.tableSortStep = p
+    },
+    SORT_TABLE_BY(s, p) {
+        s.sortTableBy = p
+    },
 }
