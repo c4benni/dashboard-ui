e36af4068c9ba5d1f1b9e893b50732c41a9f7e59
c4benni <c4benni@gmail.com>
2022-02-09 23:58:37 +0100

    Create Spacer.vue

---
+++ components/Spacer.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'Spacer',
+  functional: true,
+  props: {
+    tag: {
+      type: String,
+      default: 'div',
+    },
+  },
+  render(h, c) {
+    return h(c.props.tag, {
+      attrs: {
+        'aria-hidden': 'true',
+      },
+      staticClass: 'flex-shrink-0 flex-grow',
+    })
+  },
+}
+</script>
