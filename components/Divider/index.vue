d4ecf5b76ad83f540ce2ebba7d524b0703b4328d
c4benni <c4benni@gmail.com>
2022-02-09 13:24:24 +0100

    Update Divider
    Replaced bg-color with border-color

--- components/Divider/index.vue
+++ components/Divider/index.vue
@@ -1,11 +1,9 @@
 <template functional>
-  <hr
-    class="h-[1px] border-none bg-gray-200"
-  />
+  <hr class="h-[1px] border-0 border-b border-gray-200">
 </template>
 
 <script>
 export default {
-    name:'Divider'
+  name: 'Divider',
 }
 </script>
