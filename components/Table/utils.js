4301877edba31594080e9854451d052b819076a6
c4benni <c4benni@gmail.com>
2022-02-09 13:26:25 +0100

    Create Table/utils
    For storing information that might be shared. Might remove later.

---
+++ components/Table/utils.js
@@ -0,0 +1,10 @@
+const th = (title, width, hidden) => ({ title, width, hidden })
+
+export const thead = [
+    th('Company', 'w-119 md:w-185'),
+    th('License use', 'w-71'),
+    th('Status', 'w-[121px]'),
+    th('Users', 'w-86'),
+    th('About', 'w-[301px]'),
+    th('Actions', null, true),
+]
