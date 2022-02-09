4a03f3064a0b8c658f38de1af4c7db4cf8c96008
c4benni <c4benni@gmail.com>
2022-02-10 00:00:58 +0100

    Update actions.js

--- store/actions.js
+++ store/actions.js
@@ -1,3 +1,7 @@
+import { distinctArray } from '~/components/utils'
+
+const selectTableMutation = 'SELECTED_TABLE_ITEM_INDEX'
+
 export default {
     updateBreakpoint({ commit, state }, payload) {
         const mutation = 'BREAKPOINT'
@@ -17,5 +21,47 @@ export default {
                 isMobile: /xxs|xs|sm/.test(payload.is),
             })
         }
+    },
+    toggleTableSelection({ commit, state }, payload) {
+        if (typeof payload === 'number') {
+            const State = state.selectedTableItemIndex
+
+            if (State.includes(payload)) {
+                const newValue = State.filter((x) => x !== payload)
+
+                commit(selectTableMutation, newValue)
+            } else {
+                const newValue = distinctArray([...State, payload])
+
+                commit(selectTableMutation, newValue)
+            }
+        } else {
+            throw new TypeError(`Expected number, got ${typeof payload}`)
+        }
+    },
+    selectMultipleTableItems({ commit, state }, payload) {
+        if (Array.isArray(payload)) {
+            const newValue = distinctArray([
+                ...state.selectedTableItemIndex,
+                ...payload,
+            ])
+
+            commit(selectTableMutation, newValue)
+        } else {
+            throw new TypeError(`Expected Array, got ${typeof payload}`)
+        }
+    },
+    clearTableSelection({ commit }) {
+        commit(selectTableMutation, [])
+    },
+    tableSortStep({ commit }, payload) {
+        if (typeof payload === 'number') {
+            commit('TABLE_SORT_STEP', payload)
+        } else {
+            throw new TypeError(`Expected Number, got ${typeof payload}`)
+        }
+    },
+    sortTableBy({ commit }, payload) {
+        commit('SORT_TABLE_BY', payload)
     },
 }
