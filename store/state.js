4d30ab1ec4164aee69b18ab018353f3a39399d75
c4benni <c4benni@gmail.com>
2022-02-10 00:01:06 +0100

    Update state.js

--- store/state.js
+++ store/state.js
@@ -2,4 +2,8 @@ export default () => ({
     breakpoint: {
         inactive: true,
     },
+    selectedTableItemIndex: [],
+    // 1 = asc; 2 = desc; 0 = none;
+    tableSortStep: 0,
+    sortTableBy: null,
 })
