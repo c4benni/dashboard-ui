edd8c708783b560de617656c89a105bfd4807e6b
c4benni <c4benni@gmail.com>
2022-02-10 20:44:04 +0100

    Update Table/utils

--- components/Table/utils.js
+++ components/Table/utils.js
@@ -2,9 +2,9 @@ const th = (title, width, hidden) => ({ title, width, hidden })
 
 export const thead = [
     th('Company', 'w-119 md:w-185'),
-    th('License use', 'w-71'),
+    th('License use', 'w-68'),
     th('Status', 'w-[121px]'),
     th('Users', 'w-86'),
     th('About', 'w-[301px]'),
-    th('Actions', null, true),
+    th('Actions', null, 'w-58'),
 ]
