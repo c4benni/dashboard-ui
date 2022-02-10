49fe47f25c8dd6817a9abdffe2d64b029db9f0b8
c4benni <c4benni@gmail.com>
2022-02-10 20:40:04 +0100

    Create DashboardHeader

---
+++ components/Dashboard/Overview/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header
+    aria-label="Overview header"
+    class="max-w-7xl h-32 w-full px-16 flex items-center"
+  >
+    <Nav>
+      <NavItem
+        v-for="link in links"
+        :key="link.title"
+        :to="link.to"
+        :label="link.title"
+        :current="`/dashboard/${link.to}` === $route.path"
+      />
+    </Nav>
+
+    <Spacer />
+
+    <Search v-model="search" class="w-160" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'DashboardOverviewHeader',
+  data: () => ({
+    search: '',
+  }),
+  computed: {
+    links() {
+      const link = (title) => ({ title, to: `${title.toLowerCase().replace(/\s+/g,'-')}` })
+
+      return [
+        link('Overview'),
+        link('Notifications'),
+        link('Analytics'),
+        link('Saved reports'),
+        link('Trade history'),
+        link('User reports'),
+      ]
+    },
+  },
+}
+</script>
+
+<style></style>
