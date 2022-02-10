e1c8317f566a710efeb64b1a50d5f03c2543359b
c4benni <c4benni@gmail.com>
2022-02-10 20:41:32 +0100

    Update Img
    Used cloudinary

--- components/Img/index.vue
+++ components/Img/index.vue
@@ -1,11 +1,38 @@
 <template>
-    <img
-    
-    />
+  <img :src="publicId" :alt="alt" :width="width" :height="height" class="text-[transparent] text-[1px]"/>
 </template>
 
 <script>
+import { Cloudinary } from 'cloudinary-core'
+
+import { requiredStringProp, undefinedStringProp } from '../utils'
+
+const cloudinary = new Cloudinary({ cloud_name: 'c4benn' })
+
 export default {
-name:'Img'
+  name: 'Img',
+  props: {
+    alt: requiredStringProp,
+    src: requiredStringProp,
+    width: undefinedStringProp,
+    height: undefinedStringProp,
+    quality: undefinedStringProp,
+    loading: undefinedStringProp,
+  },
+  computed: {
+    publicId() {
+      const { src, height, width, quality } = this
+
+      return cloudinary.url(`untitledUi/${src}`, {
+        transformation: [
+          {
+            height,
+            width,
+            quality,
+          },
+        ],
+      })
+    },
+  },
 }
 </script>
