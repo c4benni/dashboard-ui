6cc6ff42ea8d189576f59f42bacef3822cc935ad
c4benni <c4benni@gmail.com>
2022-02-10 20:39:20 +0100

    Update Button

--- components/Button/index.vue
+++ components/Button/index.vue
@@ -72,7 +72,13 @@ export default {
           'font-medium items-center outline-none justify-center flex-row select-none',
         class: [
           {
-            [`${this.getSize === 'custom' ? '' : 'rounded'} whitespace-nowrap`]: !this.isLink,
+            [`${
+              this.getSize === 'custom'
+                ? ''
+                : this.pill
+                ? 'rounded-full'
+                : 'rounded'
+            } whitespace-nowrap`]: !this.isLink,
             'cursor-pointer transition-[opacity,box-shadow] active:opacity-80 can-hover:active:opacity-90':
               !this.disabled,
             'cursor-not-allowed': this.disabled,
