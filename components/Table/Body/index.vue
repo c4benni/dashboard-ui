3b6cb2a9d424612dd77cbd0255f2b80b3eba110a
c4benni <c4benni@gmail.com>
2022-02-10 20:42:16 +0100

    Update Table/Body
    Added transition on desktop for sorting

--- components/Table/Body/index.vue
+++ components/Table/Body/index.vue
@@ -1,47 +1,57 @@
 <template>
-  <tbody class="border-gray-200 border-y">
-    <TableBodyItem
-      v-for="(item, i) in content"
-      :key="item.company.name + i"
-      :index="i"
-      v-bind="item"
-      :gray="(i + 1) % 2 !== 0"
-    />
-  </tbody>
+  <Transition v-bind="transitionClasses">
+    <tbody
+      :key="breakpoint.isMobile ? undefined : tableSortStep"
+      class="border-gray-200 border-y transition-[opacity,transform]"
+    >
+      <TableBodyItem
+        v-for="(item, i) in contents"
+        :key="item.company.name + i"
+        :index="i"
+        v-bind="item"
+        :gray="(i + 1) % 2 !== 0"
+      />
+    </tbody>
+  </Transition>
 </template>
 
 <script>
-const content = [
-  {
-    logo: {
-      src: '/img/logo/Avatar.png',
-      alt: '',
-    },
-    company: {
-      name: 'Catalog',
-      website: 'catalogapp.io',
-    },
-    licenseUse: 70,
-    status: {
-      success: true,
-      label: 'Customer',
-    },
-    avatarGroup: Array.from({ length: 9 }, (_, i) => ({
-      src: `/img/Avatar (${i + 1}).png`,
-      alt: 'avi',
-    })),
-    about: {
-      title: 'Content curating app',
-      subtitle: 'Brings all your news into one place',
-    },
-  },
-]
+import { mapState } from 'vuex'
 
 export default {
   name: 'Tablebody',
 
-  data:()=>({
-    content: [...content, ...content],
-  })
+  computed: {
+    ...mapState(['tableData', 'tableSortStep','breakpoint']),
+    contents() {
+      if (this.tableSortStep === 2) {
+        return [...this.tableData].sort((a, b) => {
+          const first = a.company.name.toLowerCase()
+
+          const next = a.company.name.toLowerCase()
+
+          return first > next ? 1 : -1
+        })
+      }
+      return this.tableData
+    },
+    transitionClasses() {
+      if(this.breakpoint.isMobile) return {}
+
+      const desc = this.tableSortStep === 2
+      const down = 'translate-y-1'
+      const up = '-translate-y-1'
+      const willChange = 'will-change-[transform,opacity]'
+
+      return {
+        enterClass: `opacity-0 ${desc ? down : up} ${willChange}`,
+        enterActiveClass: `duration-[300ms] ${willChange}`,
+        leaveActiveClass: `duration-[75ms] ${willChange}`,
+        leaveClass: willChange,
+        leaveToClass: `opacity-0 ${desc ? up : down}`,
+        mode: 'out-in',
+      }
+    },
+  },
 }
 </script>
