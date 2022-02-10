9c70306e7e447f4290dc4054cd2f9d8e90471253
c4benni <c4benni@gmail.com>
2022-02-10 20:45:04 +0100

    Create Dashboard root page

---
+++ pages/dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Divider />
+
+    <template v-if="!breakpoint.isMobile">
+      <DashboardOverviewHeader />
+
+      <Divider />
+    </template>
+
+    <NuxtChild />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'DashboardPage',
+  computed: {
+    ...mapState(['breakpoint']),
+  },
+}
+</script>
+
+<style></style>
