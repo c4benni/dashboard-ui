f6abeac4cf17322d8b3e75d13fbc8cc15a60da1c
c4benni <c4benni@gmail.com>
2022-02-10 21:14:27 +0100

    Update Default Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,11 @@
       <Nuxt />
     </KeepAlive>
 
-    <div v-if="!appMounted" class="flex justify-center items-center fixed inset-0 z-10 bg-white">
-      <Icon name='spinner' size='56' class="animate-spin text-gray-500"/>
+    <div
+      v-if="!appMounted"
+      class="flex justify-center items-center fixed inset-0 z-10 bg-white"
+    >
+      <Icon name="spinner" size="56" class="animate-spin text-gray-500" />
 
       <p class="sr-only">Loading metrics...</p>
     </div>
@@ -20,24 +23,25 @@ import { initBreakpoint } from '~/components/utils'
 
 export default {
   name: 'DefaultLayout',
+
   async fetch() {
     await this.getUser()
   },
-  head(){
+  head() {
     return {
-      htmlAttrs:{
-        'data-app-loading': this.appMounted ? undefined : ''
-      }
+      htmlAttrs: {
+        'data-app-loading': this.appMounted ? undefined : '',
+      },
     }
   },
-  computed:{
-    ...mapState(['appMounted'])
+  computed: {
+    ...mapState(['appMounted']),
   },
-  beforeMount(){
+  beforeMount() {
     initBreakpoint.call(this)
   },
-  mounted(){
-    this.setAppMounted(true);
+  mounted() {
+    this.setAppMounted(true)
   },
   methods: {
     ...mapActions(['getUser', 'setAppMounted']),
