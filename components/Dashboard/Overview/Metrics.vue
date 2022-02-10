85a5b33ebea3e84e5c5c25b7aa4799094470a7f0
c4benni <c4benni@gmail.com>
2022-02-10 20:40:18 +0100

    Create Dashboard Metrics

---
+++ components/Dashboard/Overview/Metrics.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="mt-16 px-8 md:px-0">
+    <MetricGroup v-if="metrics" class="fade-appear">
+      <MetricItem
+        v-for="metric in metrics"
+        :key="metric.title"
+        v-bind="metric"
+      />
+    </MetricGroup>
+
+    <div v-else class="flex justify-center h-79 md:h-88 items-center">
+      <Icon name='spinner' size='56' class="animate-spin text-gray-500"/>
+
+      <p class="sr-only">Loading metrics...</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import { getMetrics } from '~/services'
+
+export default {
+  name: 'DashboardOverviewMetrics',
+  data: () => ({
+    metrics: null,
+  }),
+  async fetch() {
+    this.metrics = await getMetrics()
+  },
+}
+</script>
