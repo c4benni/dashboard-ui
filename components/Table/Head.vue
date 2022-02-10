944174128737099b1c7ae6812a5c26ac847ebea9
c4benni <c4benni@gmail.com>
2022-02-10 20:43:17 +0100

    Update Table/Head

--- components/Table/Head.vue
+++ components/Table/Head.vue
@@ -1,14 +1,14 @@
 <template>
-  <thead>
+  <thead class="w-full">
     <tr class="flex items-center">
       <th
         v-for="(th, i) in tableHead"
         :key="th.title"
         role="columnheader"
         scope="col"
-        :aria-sort="sortBy === th.title ? thAriaSort : 'none'"
-        :aria-label="`${th.title}. Click to ${
-          sortBy === th.title ? thAriaLabelSuffix : 'sort ascending'
+        :aria-sort="i === 0 ? thAriaSort : 'none'"
+        :aria-label="`${th.title}.${
+          i === 0 ? ` Click to ${thAriaLabelSuffix}` : ''
         }`"
         class="inline-flex items-center px-12 py-6 flex-shrink-0"
         :class="th.width"
@@ -24,8 +24,11 @@
           />
 
           <div
-            class="cursor-pointer flex items-center group"
-            @click="changeSort(th)"
+            class="flex items-center group"
+            :class="[{ 'cursor-pointer': i === 0, 'cursor-default': i !== 0 }]"
+            v-on="i === 0 ? {
+              click: ()=>changeSort(th)
+            } : {} "
           >
             <UiText
               tag="span"
@@ -38,7 +41,7 @@
             <ClientOnly>
               <Transition v-bind="arrowTransition">
                 <Icon
-                  v-if="sortBy === th.title && sortStep"
+                  v-if="i === 0 && sortStep"
                   :key="sortStep"
                   name="SortArrow"
                   size="16"
@@ -48,13 +51,6 @@
                       'rotate-180': sortStep === 2,
                     },
                   ]"
-                />
-
-                <Icon
-                  v-else-if="!breakpoint.isMobile"
-                  name="SortArrow"
-                  size="12"
-                  class="mx-2 text-gray-500 transition-[opacity,transform] transform-gpu opacity-[0] can-hover:group-hover:opacity-50"
                 />
               </Transition>
             </ClientOnly>
@@ -84,25 +80,25 @@ export default {
   }),
 
   computed: {
-    ...mapState(['selectedTableItemIndex', 'breakpoint']),
+    ...mapState(['selectedTableItemIndex', 'breakpoint', 'tableData']),
     ...mapState({
       // 1 = asc, 2 = desc, 0 = null;
       sortStep: 'tableSortStep',
-      sortBy: 'sortTableBy'
     }),
     checkboxChecked() {
-      return this.selectedTableItemIndex.length === 2
+      return this.selectedTableItemIndex.length === this.tableData.length
     },
     checkboxindeterminate() {
       return (
         this.selectedTableItemIndex.length &&
-        this.selectedTableItemIndex.length !== 2
+        this.selectedTableItemIndex.length !== this.tableData.length
       )
     },
     checkboxTitle() {
       return ''
       // return !this.selected.length ? 'Select all' : 'Clear selection'
     },
+    // fade out on leave, slide from down or up depending on sortBy state
     arrowTransition() {
       const properties = 'transition-[opacity,transform]'
       const willChange = 'will-change-transform'
@@ -150,33 +146,22 @@ export default {
     },
   },
 
-  mounted() {
-    this.tableSortStep(1)
-    this.sortTableBy('Company')
-  },
-
   methods: {
     ...mapActions([
       'clearTableSelection',
       'selectMultipleTableItems',
       'tableSortStep',
-      'sortTableBy'
     ]),
     changeSort(th) {
-      if (this.sortBy === th.title) {
-        if (this.sortStep === 2) {
-          this.tableSortStep(0)
-        } else this.tableSortStep(this.sortStep + 1)
-      } else {
-        this.sortTableBy(th.title)
-
+      if (this.sortStep === 2) {
         this.tableSortStep(1)
-      }
+      } else this.tableSortStep(2)
     },
 
     handleSelect(evt) {
       if (evt === true) {
-        this.selectMultipleTableItems([0, 1])
+        // return all indexes of tableData
+        this.selectMultipleTableItems(this.tableData.map((_, i) => i))
       } else {
         this.clearTableSelection()
       }
