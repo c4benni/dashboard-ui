da6dd24de0430974584028dd9c77d9bf0ca95666
c4benni <c4benni@gmail.com>
2022-02-12 16:51:07 +0100

    Create HeaderProfileButton

---
+++ components/AppHeader/ProfileButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    title="Profile"
+    class="w-20 h-20 rounded-full cursor-pointer bg-primary-50 can-hover:hover:opacity-90 active:opacity-80 can-hover:active:opacity-80 transition-opacity outline-none focus:ring-4 ring-primary-50"
+  >
+    <Img
+      v-if="user"
+      :src="user.avatar"
+      alt="Profile image"
+      width="40"
+      height="40"
+      class="object-cover"
+    />
+  </button>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'ProfileButton',
+  async fetch() {
+    await this.getUser()
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+  methods: {
+    ...mapActions(['getUser']),
+  },
+}
+</script>
+
+<style></style>
